234dba952b561afdca7f3e61282d078f4c0d5111
Yuki Hirai <yukihirai0505@gmail.com>
2017-12-24 17:39:52 +0900

    Update twitter account name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
       <div id="footer">
         <div className="container 75%">
           <ul className="icons">
-            <li><a href="https://twitter.com/yuukying" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+            <li><a href="https://twitter.com/yabaiwebyasan" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
             <li><a href="https://www.facebook.com/yukyhirai" className="icon fa-facebook"><span className="label">Facebook</span></a>
             </li>
             <li><a href="https://github.com/yukihirai0505" className="icon fa-github"><span className="label">Github</span></a></li>
